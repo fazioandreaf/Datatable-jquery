521579e697b4c73744083353ebe539fbf7e1b896
Andrea Fazio <fazioandrea.f@gmail.com>
2021-08-09 11:40:07 +0200

    tabella impostata

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 function init(){
     console.log('ciao');
+    let option={};
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(r=>r.json())
-    .then(data=>console.log(data))
-    const option={};
-    $('#table').DataTable(option);
+    .then(data=>{
+        let columns=[];
+        for(key in data[0]){
+            if(data[0].hasOwnProperty(key)){
+                columns.push({'data':key} )
+            }
+        }
+        let data_table=[];
+        data.forEach(elem => {   
+            let data_single={};        
+            for(key in elem){
+                if(elem.hasOwnProperty(key)){
+                    let nome=key;
+                    data_single[nome]=elem[key];
+                }
+            }        
+            data_table.push(data_single);
+        });
+        $('#table').DataTable( {
+            data: data_table,
+            columns: columns
+        } );
+
+    });
 }
 $(document).ready(init());

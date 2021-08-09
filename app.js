02c1ab097ffe5258de8e2a8b5f9789e0813d72b2
Andrea Fazio <fazioandrea.f@gmail.com>
2021-08-09 10:00:57 +0200

    call placeholder api

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('ciao')
+function init(){
+    console.log('ciao');
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(r=>r.json())
+    .then(data=>console.log(data))
+    const option={};
+    $('#table').DataTable(option);
+}
+$(document).ready(init());
